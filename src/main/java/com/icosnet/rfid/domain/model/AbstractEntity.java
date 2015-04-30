00a22b5d8a9e4eb766a65ce5d9bee086d8a53a69
zirconias <zirconias777@yahoo.com>
2015-04-30 19:11:42 +0100

    abstract entity

---
+++ src/main/java/com/icosnet/rfid/domain/model/AbstractEntity.java
@@ -0,0 +1,31 @@
+package com.icosnet.rfid.domain.model;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.*;
+/**
+ * Created by xirconias on 30/04/15.
+ */
+@MappedSuperclass
+public abstract class AbstractEntity implements Serializable{
+
+    private static final long   serialVersionUID=1L;
+    private Long id;
+    private Timestamp creationDate;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID",nullable = false)
+    public Long getId(){return id;}
+
+    public void setId(Long id){this.id=id;}
+
+    @Column(name = "CREATION_DATE",nullable = false)
+    public Timestamp getCreationDate(){return creationDate;}
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+}
