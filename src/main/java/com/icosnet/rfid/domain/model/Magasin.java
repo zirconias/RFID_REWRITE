f193252d7ce70898d28211b24b134f28de8aa658
zirconias <zirconias777@yahoo.com>
2015-05-02 23:31:55 +0100

    bad commit

--- src/main/java/com/icosnet/rfid/domain/model/Magasin.java
+++ src/main/java/com/icosnet/rfid/domain/model/Magasin.java
@@ -2,6 +2,7 @@ package com.icosnet.rfid.domain.model;
 
 import com.icosnet.rfid.util.MagasinConverter;
 import com.icosnet.rfid.util.MagasinType;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 
@@ -16,21 +17,60 @@ public class Magasin extends AbstractEntity {
     private String descr;
     private MagasinType type;
     private String localisation;
+    private String localisation2;
 
+    protected Magasin(){}
+
+    /**
+     * Creates a new {@link Magasin} from the given libelle,descr, location and type.
+     *
+     * @param libelle must not be {@literal null} or empty.
+     * @param descr must not be {@literal null} or empty.
+     * @param localisation must not be {@literal null} or empty.
+     * @param type must not be null.
+     */
+    public Magasin(String libelle, String descr, MagasinType type, String localisation) {
+        Assert.hasText(libelle);
+        Assert.hasText(descr);
+        Assert.hasText(localisation);
+        Assert.notNull(type);
+
+        this.libelle = libelle;
+        this.descr = descr;
+        this.type = type;
+        this.localisation = localisation;
+    }
+
+    /**
+     * gets  {@link Magasin}'s libelle
+     * @return
+     */
     @Column(name = "LIBELLE", nullable = false)
     public String getLibelle() {
         return libelle;
     }
 
+    /**
+     * sets {@link Magasin}'s libelle
+     * @param libelle
+     */
     public void setLibelle(String libelle) {
         this.libelle = libelle;
     }
 
+    /**
+     * returns {@link Magasin}'s description
+     * @return
+     */
     @Column(name = "DESCR")
     public String getDescr() {
         return descr;
     }
 
+    /**
+     * sets {@link Magasin}'s description
+     * @param descr
+     */
     public void setDescr(String descr) {
         this.descr = descr;
     }
@@ -41,15 +81,28 @@ public class Magasin extends AbstractEntity {
         return type;
     }
 
+    /**
+     * sets {@link Magasin}'s type
+     * @param type
+     */
     public void setType(MagasinType type) {
         this.type = type;
     }
 
+    /**
+     * returns {@link Magasin}'s location
+     *
+     * @return
+     */
     @Column(name = "LOCATION")
     public String getLocalisation() {
         return localisation;
     }
 
+    /**
+     * sets {@link Magasin}'s location
+     * @param localisation
+     */
     public void setLocalisation(String localisation) {
         this.localisation = localisation;
     }
