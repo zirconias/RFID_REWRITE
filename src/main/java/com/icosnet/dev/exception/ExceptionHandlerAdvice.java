f04e8704726e7cc4f225fa50f53d73426e4a0609
zirconias <zirconias777@yahoo.com>
2015-05-20 10:34:56 +0100

    aa

---
+++ src/main/java/com/icosnet/dev/exception/ExceptionHandlerAdvice.java
@@ -0,0 +1,10 @@
+package com.icosnet.dev.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by xirconias on 20/05/15.
+ */
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+}
